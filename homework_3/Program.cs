40dca3b8ce681b5663c6dc18e68a5ec78fc82507
Aizhan <ajmuhametzanova@gmail.com>
2022-08-16 01:08:14 +0300

    add Homework 3

---
+++ homework_3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот деньConsole.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("ВЫХОДНОЙ!!!");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Хмм, в неделе 7 дней.");
+  }
+  else Console.WriteLine("Упс ,это рабочие будни(.");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
